9a6790d2bbabbe169381c87d143c7af0e295740a
unknown <davyberra@gmail.com>
2021-01-15 21:00:56 -0800

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Dog dog = new Dog();
+	dog.makeSound();
+    }
+}
+
+class Animal {
+    String sound = "";
+
+    void makeSound() {
+        System.out.println(sound);
+    }
+}
+
+class Dog extends Animal {
+    Dog() {
+        sound = "bark";
+    }
+}
