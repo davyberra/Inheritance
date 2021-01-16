0470cf44e24d4be0bd06d15acc2afd38bd420d02
unknown <davyberra@gmail.com>
2021-01-15 22:06:18 -0800

    Finished Inheritance Lesson

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,23 +1,65 @@
 package com.company;
 
+import java.util.Objects;
+
 public class Main {
 
     public static void main(String[] args) {
-	Dog dog = new Dog();
-	dog.makeSound();
+	    Dog dog1 = new Dog("bark");
+        Dog dog2 = new Dog("bark");
+        System.out.println(dog1.equals(dog2));
+
     }
 }
 
-class Animal {
+abstract class Animal {
     String sound = "";
+
+    Animal(String sound) {
+        this.sound = sound;
+    }
+
+    abstract void findFood();
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + ": sound = " + sound;
+    }
 
     void makeSound() {
         System.out.println(sound);
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Animal animal = (Animal) o;
+        return Objects.equals(sound, animal.sound);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(sound);
+    }
 }
 
 class Dog extends Animal {
-    Dog() {
-        sound = "bark";
+    Dog(String sound) {
+        super(sound);
+    }
+
+    @Override
+    void findFood() {
+        System.out.println("*looks at human*");
+        makeSound();
+    }
+
+    @Override
+    void makeSound() {
+        super.makeSound();
+        System.out.println("*wags tail*");
     }
 }
+
+class DogFood {}
